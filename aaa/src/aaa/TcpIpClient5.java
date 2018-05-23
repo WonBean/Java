5fe26abc5dbea6ba443c763b8eb996ccabfc873b
riuhwan <riuhwan@gmail.com>
2018-05-23 20:50:19 +0900

    Binggo

---
+++ aaa/src/aaa/TcpIpClient5.java
@@ -0,0 +1,28 @@
+package aaa;
+
+import java.io.*;
+import java.net.*;
+
+public class TcpIpClient5 {
+
+	public static void main(String[] args) {
+		try {
+			String serverIp = "10.10.10.141";
+			Socket socket = new Socket(serverIp, 7777);
+
+			System.out.println("서버에 연결되었습니다.");
+			Sender sender = new Sender(socket);
+			Receiver receiver = new Receiver(socket);
+
+			sender.start();
+			receiver.start();
+		} catch (ConnectException ce) {
+			ce.printStackTrace();
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
